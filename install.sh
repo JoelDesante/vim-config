9b58eea5508b4388a15fc4e72042a232c0472851
jdesante <jdesante@cs.odu.edu>
2021-01-26 19:33:21 -0500

    Finnished install script

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+# Install Plug-Vim (https://github.com/junegunn/vim-plug) to manage plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Copy in the vim configuration
+wget -O ~/.vimrc https://raw.githubusercontent.com/JoelDesante/vim-config/master/.vimrc
+
+# Install the plugins
+vim +PlugInstall +qall
